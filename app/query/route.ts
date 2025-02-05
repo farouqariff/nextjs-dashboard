c0ca94a2ff890e7cb55164ca15b24ad2f653b52d
Farouq Ariff <farouqariff4399@gmail.com>
2025-02-05 14:14:48 +0800

    done

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,6 @@
 // import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: false });
 
 // async function listInvoices() {
 // 	const data = await sql`
@@ -16,7 +16,7 @@
 export async function GET() {
   return Response.json({
     message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+      "Uncomment this file and remove this line. You can delete this file when you are finished.",
   });
   // try {
   // 	return Response.json(await listInvoices());
